3f033cf0eab584f269aab4db1b55a7bf94116e1e
Dong Woo Jung <dewjay128@gmail.com>
2021-05-25 09:55:24 -0400

    constants

--- src/Components/constants.js
+++ src/Components/constants.js
@@ -1,6 +1,6 @@
-export const TWENTY_FIVE_MIN = 5;
-export const FIVE_MIN = 5;
-export const FIFTEEN_MIN = 5;
+export const TWENTY_FIVE_MIN = 25 * 60;
+export const FIVE_MIN = 5 * 60;
+export const FIFTEEN_MIN = 15 * 60;
 
 export const POMODORO = "POMODORO";
 export const SHORT_BREAK = "SHORT_BREAK";
